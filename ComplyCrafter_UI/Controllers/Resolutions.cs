1ed8afcf333c7ab4aae6ddc52628cd582ebcc0bf
OHA2025g <dt@692664@gamil.com>
2025-11-22 04:51:27 +0530

    Update2.0

--- ComplyCrafter_UI/Controllers/Resolutions.cs
+++ ComplyCrafter_UI/Controllers/Resolutions.cs
@@ -205,19 +205,578 @@ namespace ComplyCrafter_UI.Controllers
         }
 
         
+
+      public IActionResult To_issue_and_allot_shares_as_envisaged_in_the_scheme_of_arrangement__merger__demerger_approved_by_Honble_NCLT_()
+       {
+            return View();
+       }
+      public IActionResult To_recommend_buy_back_of_()
+      {
+       return View();
+      }
+      public IActionResult To_approve_the_allotment_of_upon_conversion_of_()
+      {
+       return View();
+      }
+           public IActionResult To_approve_Transmission_of_()
+      {
+       return View();
+      }
+      public IActionResult To_consider_and_approve_transfer_of_()
+      {
+       return View();
+      }
+      public IActionResult To_take_note_transferred_in_demat_form()
+      {
+       return View();
+      }
+      public IActionResult To_take_note_of_Transmission_of_in_demat_form()
+      {
+       return View();
+      }
+      public IActionResult To_approve_the_alteration_of_the_article_of_association_of_the_company()
+      {
+       return View();
+      }
+      public IActionResult To_Adopt_memorandum_and_Article_of_Association_of_the_company()
+      {
+       return View();
+      }
+      public IActionResult To_Adopt_the_new_set_of_articles_of_association()
+      {
+       return View();
+      }
+
+      public IActionResult To_take_note_on_the_resignation_of_Chief_Executive_Officer_KMP_of_the_Company()
+      {
+       return View();
+      }
+         public IActionResult To_take_note_on_the_resignation_of_Chief_Financial_Officer_KMP_of_the_Company()
+      {
+       return View();
+      }
+          public IActionResult To_consider_the_Special_Notice_received_under_Section_115_of_the_Companies_Act_2013_proposing_the_removal_of_()
+      {
+       return View();
+      }
+
+
+
+        public IActionResult To_consider_the_written_representation_received_from_in_response_to_the_Special_Notice_received_proposing_removal()
+        {
+            return View();
+        }
+
+          public IActionResult To_present_deep_condolences_at_the_sudden__untimely_demise_of__()
+        {
+            return View();
+        }
+
+           public IActionResult To_propose_removal_of_a_()
+        {
+            return View();
+        }
+
+            public IActionResult To_take_note_on_the_resignation_of_()
+        {
+            return View();
+        }
+
+
+             public IActionResult To_vacate_office_of_Alternate_()
+        {
+            return View();
+        }
+
+           public IActionResult To_take_note_of_Cessation_of__from_the_post_of_of_the_Company()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        public IActionResult To_alter_the_article_of_association_to_convert_the_Company_from_a_private_limited_company_to_a_public_limited_company()
+        {
+            return View();
+        }
+        public IActionResult To_Adopt_new_set_of_Memorandum_of_Association_of_Company()
+        {
+            return View();
+        }
+        public IActionResult To_change_the_name_of_company_to_convert_the_Company_from_a_private_limited_company_to_a_public_limited_company()
+        {
+            return View();
+        }
+
+        public IActionResult To_take_a_note_of_Disclosure_of_Interest_pursuant_to_the_provision_of_Section_184_of_the_Companies_Act_2013()
+        {
+            return View();
+        }
+
+      
+
+           public IActionResult To_take_a_note_of_Directors_Nondisqualification_us_164()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_avail_unsecured_loan_from_s()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_inform_the_Board_about_the_duties_of_the_()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_take_note_on_the_declaration_received_from_independent_()
+        {
+            return View();
+        }
+
+      
+           public IActionResult To_revise_the_remuneration_of__of_the_Company()
+        {
+            return View();
+        }
+
+          public IActionResult To_fix_the_remuneration_of_()
+        {
+            return View();
+        }
+
+       public IActionResult To_provide_consent_for_verifying_DIN_Application_of_proposed_appointee_()
+        {
+            return View();
+        }
+         public IActionResult To_approve_the_Change_in_designation_of_from_to_()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_Redesignate_as_Chief_Financial_officer()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_recommend_regularization_of_Additional_as_Director_of_the_Company()
+        {
+            return View();
+        }
+
+      
+        public IActionResult To_Redesignate_as_Chief_Executive_officer()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_Cancel_the_unissued_()
+        {
+            return View();
+        }
+
+      
+
+         public IActionResult To_approve_Change_in_name_clause()
+        {
+            return View();
+        }
+
+        public IActionResult To_alter_the_object_clause_of_the_company_by_substitution_of_certain_existing_clause()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_alter_the_object_clause_of_the_company_by_addition_of_new_clause()
+        {
+            return View();
+        }
+
+      
+
+          public IActionResult To_recommend_Conversion_of_private_company_into_public_company()
+        {
+            return View();
+        }
+
+        public IActionResult To_discuss_and_consider_the_appointment_of_company_law_consultants_for_the_company()
+        {
+            return View();
+        }
+
+        public IActionResult To_discuss_and_authorize_a_for_filing_of_various_eforms_with_the_Ministry_of_Corporate_Affairs()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_Print_new_stationery_and_painting_of_new_name_plates_as_per_Section_12_C_of_Companies_Act_2013()
+        {
+            return View();
+        }
+
+        public IActionResult To_Appoint_a_person_as_an_officer_in_default()
+        {
+            return View();
+        }
+
+       public IActionResult To_approve_the_withdrawal_of_consent_by_the_person_who_has_been_designated_as_officer_in_default()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_sell_lease_or_otherwise_dispose_of_the_of_the_undertaking_of_the_company_as_per_the_section_1801a_of_the_companies_act_2013()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_Authorize_s_for_general_day_to_day_operation_of_the_company()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_open_Demat_account_of_the_company()
+        {
+            return View();
+        }
+
+      
+
+         public IActionResult To_take_note_of_resignation_of_from_the_post_of_of_the_company()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_take_note_of_renewal_of_Legal_Entity_Identifier_LEI()
+        {
+            return View();
+        }
+
+       public IActionResult To_authorize_for_obtaining_Legal_Entity_Identifier()
+        {
+            return View();
+        }
+
+       public IActionResult To_authorize_for_renewal_of_Legal_Entity_Identifier_LEI()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_take_a_Retail_store_on_lease_for_business_purpose()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_consider_the_renewal_of_lease_taken_of_Retail_store_for_business_purpose()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_apply_for_a_Trademark_on_behalf_of_the_Company()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_provide_consent_to_be_a_part_of_the_Group_and_Issuance_of_NOC()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_authorize_the_board_to_borrow_money_under_section_1793d_subject_to_the_limit_under_section_180()
+        {
+            return View();
+        }
+
+       public IActionResult To_consider_and_approve_availing_of_Vehicle_loan()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_grant_loan_or_give_guarantee_and_provide_security_provided_in_Section_185_of_the_Companies_Act_2013()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_make_loan__investment_and_give_guarantee__provide_security_under_section_186_of_the_companies_act_2013()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_authorize_the_board_to_borrow_money_under_section_1793d()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_raise_fund_by_issuing_()
+        {
+            return View();
+        }
+
+       public IActionResult To_allot_()
+        {
+            return View();
+        }
+
+      
+        public IActionResult To_approve_the_investment_of_Surplus_Fund_of_the_Company()
+        {
+            return View();
+        }
+
+      
+         public IActionResult To_authorize_a_Company_Cibil_check()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_authorise_for_migration_to_GST()
+        {
+            return View();
+        }
+
+      
+          public IActionResult To_make_application_for_registration_of_the_Company_with_Customs_Department_()
+        {
+            return View();
+        }
+
+      
+
+           public IActionResult To_Consider_and_Approve_Authorization_of_a_for_GST_Registration()
+        {
+            return View();
+        }
+
+      
+        public IActionResult To_Authorize_for_recording_of_proceedings_of_Meetings()
+        {
+            return View();
+        }
+
+
+        public IActionResult To_authorize_the_to_sign_and_submit_tender_application_on_behalf_of_the_company()
+        {
+            return View();
+        }
+
+
+        public IActionResult To_Authorize_for_safe_custody_of_minutes()
+        {
+            return View();
+        }
+
+
+        public IActionResult To_authorize_for_various_purpose()
+        {
+            return View();
+        }
+
+
+
+         public IActionResult To_authorize_a_to_certify_the_signed_minutes()
+        {
+            return View();
+        }
+
+
+        public IActionResult To_delegate_power_to_the_under_section_179_of_the_Companies_Act_2013()
+        {
+            return View();
+        }
+
+
+         public IActionResult To_Authorize_for_signing_of_documents_relating_to_HR_department()
+        {
+            return View();
+        }
+
+
+         public IActionResult To_Authorize_for_subscribing_for_membership_of_a_society()
+        {
+            return View();
+        }
+
+        public IActionResult Authority_to_s_of_the_Company_to_sign_the_Agreement()
+        {
+            return View();
+        }
+
+         public IActionResult Authorization_for_procuring_Orgainzation_Digital_Signature_Certificate_DSC()
+        {
+            return View();
+        }
+
+          public IActionResult To_avail_Corporate_Internet_banking()
+        {
+            return View();
+        }
+
+           public IActionResult To_authorise_for_Closing_of_Bank_Account()
+        {
+            return View();
+        }
+
+     
+            public IActionResult To_open_Bank_Account_for_the_Company()
+        {
+            return View();
+        }
+
+     
+
+           public IActionResult To_approve_the_change_in_Bank_authorized_signatory()
+        {
+            return View();
+        }
+
+
+          public IActionResult To_declare_an_Interim_Dividend()
+        {
+            return View();
+        }
+
+
+
+         public IActionResult To_propose_payment_of_dividend_on_equity_shares()
+        {
+            return View();
+        }
+
+
+         public IActionResult To_authorize_Opening_of_Bank_account_for_Dividend_Payment()
+        {
+            return View();
+        }
+
+
+
+          public IActionResult To_obtain_electricity_connection()
+        {
+            return View();
+        }
+
+
+
+           public IActionResult To_take_property_on_rent()
+        {
+            return View();
+        }
+
+
+         public IActionResult To_give_Authorization_to_get_the_company_register_with_Central_KYC_Registry()
+        {
+            return View();
+        }
         
+         public IActionResult To_consider_and_approve_the_draft_accounts_of_the_company_for_the_()
+        {
+            return View();
+        }
+
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       
+         public IActionResult To_Authorize_for_Preparation_of_Annual_return()
+        {
+            return View();
+        }
+
+
+        public IActionResult To_authorize_for_preparation_of_Boards_Report()
+        {
+            return View();
+        }
+
+         public IActionResult To_consider_and_approve_the_draft_boards_report_of_the_company_for_the_financial_year_()
+        {
+            return View();
+        }
+
+         public IActionResult To_consider_and_approve_the_draft_annual_return_of_the_company_for_the_()
+        {
+            return View();
+        }
+
+
+
+         public IActionResult To_approve_the_audited_financial_statements_for_the_year_ended_()
+        {
+            return View();
+        }
+
+
+         public IActionResult To_approve_the_Draft_Consolidated_Accounts_of_the_Company_for_the_year_ended_()
+        {
+            return View();
+        }
+
+
+
+          public IActionResult To_approve_and_adopt_the_Audited_Consolidated_Financial_Statements_of_the_Company()
+        {
+            return View();
+        }
+
+
+           public IActionResult To_Extend_the_date_of_Annual_General_Meeting()
+        {
+            return View();
+        }
+
+          public IActionResult To_make_an_application_with_ROC_for_allowing_extension_for_convening_Annual_General_Meeting()
+        {
+            return View();
+        }
+
+
+
+          public IActionResult To_Adopt_the_Secretarial_Standard()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         [HttpGet("[controller]/ViewResolution/{view}/{mode}/{meeting}/{resolnId}")]
         public IActionResult ViewResolution(string view, string mode, string meeting, string resolnId)
         {
